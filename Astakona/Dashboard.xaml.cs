ff54293b9df307ae8548185443edc2feacc05bbd
Bluie25 <andriehalim0401@gmail.com>
2023-11-07 19:49:59 +0800

    Add project files.

---
+++ Astakona/Dashboard.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Astakona
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderButtonClick(object sender, RoutedEventArgs e)
+        {
+            OrderPage orderPage = new OrderPage();
+            this.Close();
+            orderPage.Show();
+        }
+    }
+
+    
+}
