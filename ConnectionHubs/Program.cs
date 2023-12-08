1a78a39a79f6ff0c3bb28a6c18c0a30a2b3d793e
Bluie25 <118502585+Bluie25@users.noreply.github.com>
2023-12-09 04:54:52 +0700

    Final stage working program

--- ConnectionHubs/Program.cs
+++ ConnectionHubs/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
 
+builder.Configuration.AddJsonFile("appsettings.json");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +20,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -31,4 +35,4 @@ app.MapRazorPages();
 // Map SignalR hub
 app.MapHub<Hubs>("/Hubs");
 
-app.Run();
+app.Run("http://192.168.1.3:5210");
